f32d16ce879a457eacb2c9eff2da3ba4549144a2
ZornErick <erick.nogueira.zorn@gmail.com>
2023-01-25 13:14:00 -0300

    Ajuste no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import {CalculadoraIcon} from "../../assets/CalculadoraIcon";
 
 export function Header() {
     return (
-        <header className={"flex justify-start items-center lg:mx-48"}>
+        <header className={"flex justify-start items-center lg:my-12 lg:mx-48 max-lg:justify-center"}>
             <CalculadoraIcon />
             <div className={"flex flex-col lg:items-center"}>
                 <h1 className={"text-white text-lg font-bold"}>Teorema de</h1>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import {Calculadora} from "../components/Calculadora";
 import {Header} from "../components/Header";
-import {Info} from "../components/Info";
 
 export function Home() {
     return (
         <main className={"h-screen flex flex-col justify-between"}>
             <div>
-                <Info />
                 <Header />
             </div>
             <Calculadora />
